e164d0e4401f2fed569a62fdaeadca1c61037838
GamePad <hejdjdjuduf@gmail.com>
2026-05-01 18:49:31 +0300

    feat(MainWindow): add test main window design

--- ConsultPlanner/Models/Users.cs
+++ ConsultPlanner/Models/Users.cs
@@ -29,10 +29,10 @@ namespace ConsultPlanner.Models
         public string FirstName { get; set; }
         public string Patronymic { get; set; }
         public System.DateTime Birthday { get; set; }
-        public string HashPassword { get; set; }
         public string Phone { get; set; }
         public string Email { get; set; }
         public System.DateTime RegisterDate { get; set; }
+        public string Password { get; set; }
     
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<Consultants> Consultants { get; set; }
